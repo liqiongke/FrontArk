1556dbf0b9a5abc38988d86743a31f817d53c0e7
liqiongke <liqiongke@szjiuli.com>
2025-11-18 10:48:38 +0800

    feat: 初始化route

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <>666</>
+    <BrowserRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
