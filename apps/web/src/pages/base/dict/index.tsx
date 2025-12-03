2089e4640db67b08612a022c266c59ec9a30101e
liqiongke <liqiongke@szjiuli.com>
2025-12-03 12:04:00 +0800

    chore(web): 调整web应用结构

--- apps/web/src/pages/base/dict/index.tsx
+++
@@ -1,9 +0,0 @@
-const Dict = () => {
-  return (
-    <div>
-      <h1>字典页面</h1>
-    </div>
-  );
-};
-
-export default Dict;
--- apps/web/src/pages/base/setting/index.tsx
+++
@@ -1,9 +0,0 @@
-const Setting = () => {
-  return (
-    <div>
-      <h1>设置页面</h1>
-    </div>
-  );
-};
-
-export default Setting;
